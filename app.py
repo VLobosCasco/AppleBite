9418104625840802387ac0e9ecca9880f261b186
VLobosCasco <valenlobosc@gmail.com>
2025-06-17 15:56:10 -0300

    Cambio de formato

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import requests
 
 st.set_page_config(page_title="AppleBite", layout="wide")
 st.title("📱 AppleBite ")
-st.subheader("Catálogo de Productos")  # opción simple
+st.subheader("Catálogo de Productos Sellados")  # opción simple
 
 # Obtener cotización dólar blue con margen
 url = 'https://mercados.ambito.com/dolar/informal/variacion'
@@ -25,12 +25,12 @@ st.markdown(f"Cotización del dólar: **${dolar:,.2f} ARS**", unsafe_allow_html=
 
 # Datos
 data = {
-    "Producto": ["iPhone 13", "iPhone 14", "iPhone 15", "iPhone 15 Plus", "iPhone 16", "iPhone 16E", "iPhone 16 Pro",
+    "Producto": ["iPhone 13 - 128GB", "iPhone 14 - 128GB", "iPhone 15 - 128GB", "iPhone 16 - 128GB", "iPhone 16E - 128GB", "iPhone 16 Pro - 256GB",
                  "iPad 10", "iPad 11", "iPad Air 13 M2",
                  "Apple Watch SE 2 (40mm)", "Apple Watch SE 2 (44mm)", "Apple Watch S9 (41mm)", "Apple Watch S10 (42mm)",
                  "Adaptador original 20W"],
-    "Categoría": ["Celulares"] * 7 + ["Tablets"] * 3 + ["Reloj"] * 4 + ["Accesorios"],
-    "USD": [580, 690, 780, 850, 870, 685, 1050, 600, 500, 890, 310, 330, 450, 460, 45]
+    "Categoría": ["Celulares"] * 6 + ["Tablets"] * 3 + ["Reloj"] * 4 + ["Accesorios"],
+    "USD": [560, 670, 750, 850, 685, 1100, 600, 500, 890, 310, 330, 450, 460, 45]
 }
 df = pd.DataFrame(data)
 df["ARS"] = df["USD"] * dolar
@@ -52,7 +52,7 @@ thead tr th, tbody tr td {
 filtro["USD"] = filtro["USD"].apply(lambda x: f"US$ {x:,.2f}")
 filtro["ARS"] = filtro["ARS"].apply(lambda x: f"${x:,.2f}")
 
-st.dataframe(filtro, use_container_width=True)
+st.dataframe(filtro, use_container_width=True, hide_index=True)
 
 
 
